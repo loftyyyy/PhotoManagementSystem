6ae8065d2e1ebc2127b800c32425a9cf56b76936
loftyyyy <lofty08.business@gmail.com>
2025-04-17 22:31:44 +0800

    WIP: Frames

--- resources/views/photobooth.blade.php
+++ resources/views/photobooth.blade.php
@@ -4,9 +4,8 @@
         <p class="text-center mt-3 dark:text-gray-500">Choose the perfect layout to elevate your photo session — explore a variety of stunning styles and curated poses to match your unique vibe.</p>
 
         <div class="">
-            <div class="grid grid-cols-2 grid-rows-2 gap-4">
+            <div class="grid grid-cols-2 grid-rows-2 gap-4 place-items-center">
                 <div class="flex flex-col items-center  bg-gray-200 w-fit border-4 border-pink-300 rounded-3xl">
-
                     <div class="py-3 px-3">
                         <div class="mb-3">
                             <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
@@ -28,14 +27,62 @@
                         <p class="text-center text-gray-600">4 Pose</p>
                     </div>
                 </div>
-                <div class="flex flex-col items-center">
-                    <div>2</div>
+                <div class="flex flex-col items-center  bg-gray-200 w-fit h-fit border-4 border-pink-300 rounded-3xl">
+                    <div class="py-3 px-3">
+                        <div class="mb-3">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="mb-3">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+
+                    </div>
+                    <div class="bg-gray-100 w-full text-center py-3 rounded-b-3xl">
+                        <p class="text-xs text-gray-500 text-center">Sidlac 04/14/2025</p>
+                        <h3 class="text-xl font-bold text-center">Layout B</h3>
+                        <p class="text-center text-gray-600">3 Pose</p>
+                    </div>
                 </div>
-                <div class="flex flex-col items-center">
-                    <div>3</div>
+                <div class="flex flex-col items-center  bg-gray-200 w-fit h-fit border-4 border-pink-300 rounded-3xl">
+                    <div class="py-3 px-3">
+                        <div class="mb-3">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="mb-3">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+
+                    </div>
+                    <div class="bg-gray-100 w-full text-center py-3 rounded-b-3xl">
+                        <p class="text-xs text-gray-500 text-center">Sidlac 04/14/2025</p>
+                        <h3 class="text-xl font-bold text-center">Layout B</h3>
+                        <p class="text-center text-gray-600">3 Pose</p>
+                    </div>
                 </div>
-                <div class="flex flex-col items-center">
-                    <div>4</div>
+                <div class="flex flex-col items-center  bg-gray-200 w-fit h-fit border-4 border-pink-300 rounded-3xl">
+                    <div class="py-3 px-3">
+                        <div class="mb-3">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="mb-3">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+
+                    </div>
+                    <div class="bg-gray-100 w-full text-center py-3 rounded-b-3xl">
+                        <p class="text-xs text-gray-500 text-center">Sidlac 04/14/2025</p>
+                        <h3 class="text-xl font-bold text-center">Layout B</h3>
+                        <p class="text-center text-gray-600">3 Pose</p>
+                    </div>
                 </div>
             </div>
         </div>
