3bc192e00abf59adc770421ede64e295c7845ec5
loftyyyy <lofty08.business@gmail.com>
2025-04-18 23:09:01 +0800

    WIP: Functionality ng Frames

--- resources/views/photobooth.blade.php
+++ resources/views/photobooth.blade.php
@@ -4,7 +4,7 @@
         <p class="text-center mt-3 dark:text-gray-500">Choose the perfect layout to elevate your photo session — explore a variety of stunning styles and curated poses to match your unique vibe.</p>
 
         <div class="">
-            <div class="grid grid-cols-2 grid-rows-2 gap-4 place-items-center">
+            <div class="grid grid-cols-2 grid-rows-4 gap-4 place-items-center">
 {{--                Quadro Stack--}}
                 <div class="flex flex-col items-center  bg-gray-200 w-fit border-4 border-pink-300 rounded-3xl">
                     <div class="py-3 px-3">
@@ -89,6 +89,65 @@
                         <h3 class="text-xl font-bold text-center">Lumi Grid</h3>
                         <p class="text-center text-gray-600">6 Pose</p>
                     </div>
+                </div>
+
+                <div class="flex flex-col items-center  bg-gray-200 w-fit h-fit border-4 border-pink-300 rounded-3xl">
+                    <div class=" gap-4 py-3 px-3">
+                        <div class="">
+                            <img class="rounded-lg" src="https://placehold.co/360x320" alt="">
+                        </div>
+
+                    </div>
+                    <div class="bg-gray-100 w-full text-center py-3 rounded-b-3xl">
+                        <p class="text-xs text-gray-500 text-center">Sidlac 04/14/2025</p>
+                        <h3 class="text-xl font-bold text-center">Solo Star</h3>
+                        <p class="text-center text-gray-600">1 Pose</p>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center  bg-gray-200 w-fit h-fit border-4 border-pink-300 rounded-3xl">
+
+                    <div class="grid grid-cols-2 grid-rows-2 gap-4 py-3 px-3">
+                        <div>
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="col-start-1 row-start-2">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="col-start-2 row-start-1">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="bg-gray-200 w-full text-center py-3 rounded-b-3xl lg:mt-10 sm:mt-0 ">
+                            <p class="text-xs text-gray-500 text-center">Sidlac 04/14/2025</p>
+                            <h3 class="text-xl font-bold text-center">Flash Trio</h3>
+                            <p class="text-center text-gray-600">3 Pose</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center bg-gray-200 w-fit h-fit border-4 border-pink-300 rounded-3xl">
+                    <div class="grid grid-cols-3 auto-rows-auto gap-4 px-3 py-3 h-fit">
+                        <div class="col-span-2 h-fit">
+                            <img class="rounded-lg" src="https://placehold.co/280x260" alt="">
+                        </div>
+                        <div class="col-start-3 row-start-1 flex items-center justify-center">
+                            <div class="bg-gray-200 lg:mr-15 w-full text-center py-3 rounded-b-3xl">
+                                <p class="text-xs text-gray-500">Sidlac 04/14/2025</p>
+                                <h3 class="text-xl font-bold">Story Snap</h3>
+                                <p class="text-gray-600">4 Pose</p>
+                            </div>
+                        </div>
+                        <div class="row-start-2 h-fit">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="row-start-2 h-fit">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                        <div class="row-start-2 h-fit">
+                            <img class="rounded-lg" src="https://placehold.co/180x160" alt="">
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
